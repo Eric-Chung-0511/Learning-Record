8c0b0f3b6175f67246a23a52d086b6727c0d0de3
Eric_Chung <148635657+Eric-Chung-0511@users.noreply.github.com>
2024-05-27 22:01:44 +0800

    Create unzip_data.py

---
+++ General Helpers/unzip_data.py
@@ -0,0 +1,27 @@
+# download the file you want to use
+!wget http://example.com/somefile.zip
+
+# unzip the file
+import zipfile
+
+def unzip_data(filename):
+    """
+    Unzips filename into the current working directory.
+
+    Parameters:
+    filename (str): A filepath to a target zip folder to be unzipped.
+
+    Returns:
+    None
+    """
+    # Open the zip file in read mode
+    zip_ref = zipfile.ZipFile(filename, "r")
+    
+    # Extract all contents of the zip file into the current working directory
+    zip_ref.extractall()
+    
+    # Close the zip file to free up resources
+    zip_ref.close()
+
+# usage way: load unzip file
+unzip_data('somefile.zip')
