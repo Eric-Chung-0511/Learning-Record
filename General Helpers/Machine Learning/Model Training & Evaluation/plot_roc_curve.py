aef2f4f05b2f9c94566ba0d5ba411aa46dca0014
Eric_Chung <148635657+Eric-Chung-0511@users.noreply.github.com>
2024-07-18 22:03:58 +0800

    Update plot_roc_curve.py

--- General Helpers/Machine Learning/Model Training & Evaluation/plot_roc_curve.py
+++ General Helpers/Machine Learning/Model Training & Evaluation/plot_roc_curve.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 
+# plot fot binary
 def plot_roc_curve(model, X_test, y_test):
     """
     Plot ROC curve for a given model and test data.
@@ -39,3 +40,40 @@ def plot_roc_curve(model, X_test, y_test):
 
 # Example usage:
 # plot_roc_curve(svc, X_test_pca, y_test)
+
+
+# Plot for multiclass
+def plot_roc_curve_multiclass(model, X_test, y_test, n_classes=3):
+   
+    # Get the predicted probabilities for each class
+    y_pred_proba = model.predict_proba(X_test)
+
+    # Compute the log loss
+    logloss = log_loss(y_test, y_pred_proba)
+    print("Log Loss:", logloss)
+
+    # Initialize dictionaries to store false positive rates, true positive rates, and ROC AUC
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    # Compute ROC curve and ROC AUC for each class
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test, y_pred_proba[:, i], pos_label=i)
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Plot the ROC curve
+    plt.figure(figsize=(7, 5))
+    colors = ['blue', 'green', 'red']  # Change colors as needed
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                 label='ROC curve of class {0} (area = {1:0.2f})'.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], color='black', linestyle='--', lw=4)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC)')
+    plt.legend(loc="lower right")
+    plt.show()
